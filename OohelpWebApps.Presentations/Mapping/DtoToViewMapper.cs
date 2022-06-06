2418e5de3e58c7811b27e56ed685a6c093a0b2e2
Andrew Slusarev <jugius@gmail.com>
2022-06-06 14:02:32 +0300

    Передавать иконку борда кружком как Path = google.maps.SymbolPath.CIRCLE.
    #6

--- OohelpWebApps.Presentations/Mapping/DtoToViewMapper.cs
+++ OohelpWebApps.Presentations/Mapping/DtoToViewMapper.cs
@@ -6,15 +6,14 @@ namespace OohelpWebApps.Presentations.Mapping
 {
     public static class DtoToViewMapper
     {
-        const string ICON_OLD_DROP_BASE_LINK = @"https://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png";
-        const string ICON_OLD_CIRCLE_BASE_LINK = @"https://www.gstatic.com/mapspro/images/stock/959-wht-circle-blank.png";
+        const string ICON_OLD_DROP_BASE_LINK = @"https://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png";        
         const string ICON_CIRCLE_BASE_LINK = @"https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container-bg_4x.png,icons/onion/SHARED-mymaps-container_4x.png&highlight=ff000000,{0}";
         const string ICON_DROP_BASE_LINK = @"https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-pin-container-bg_4x.png,icons/onion/SHARED-mymaps-pin-container_4x.png,icons/onion/1899-blank-shape_pin_4x.png&highlight=ff000000,{0},ff000000";
         const string ICON_POINT_BASE_LINK = @"https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-measle-container-bg_4x.png,icons/onion/SHARED-mymaps-measle-container_4x.png,icons/onion/1739-blank-measle_4x.png&highlight=ff000000,{0},ff000000";
         const string ICON_ROUNDED_BILLBOARD_BASE_LINK = @"https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container-bg_4x.png,icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1612-japanese-post-office_4x.png&highlight=ff000000,{0},ff000000";
         const string ICON_ROUNDED_HOUSE_BASE_LINK = @"https://mt.google.com/vt/icon/name=icons/onion/SHARED-mymaps-container-bg_4x.png,icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1603-house_4x.png&highlight=ff000000,{0},ff000000";
         const string ICON_ARROW_PATH = "M -6,0 6,0 M 0,2 -2,6 0,4 2,6 Z ";
-        public static Models.PresentationViewModel ToPresentationViewModel(this Domain.Data.PresentationDto dto)
+        public static Models.PresentationViewModel ToPresentationViewModel(this PresentationDto dto)
         {
             return new Models.PresentationViewModel
             {
@@ -28,7 +27,7 @@ namespace OohelpWebApps.Presentations.Mapping
                 ColumnCondition = dto.ColumnCondition,              
             };
         }
-        public static Models.BoardViewModel ToBoardViewModel(this Domain.Data.BoardDto dto)
+        public static Models.BoardViewModel ToBoardViewModel(this BoardDto dto)
         {
             return new Models.BoardViewModel
             {
@@ -46,13 +45,14 @@ namespace OohelpWebApps.Presentations.Mapping
                 Icon = GenerateIcon(dto)
             };
         }
-
+        
         private static object GenerateIcon(BoardDto dto)
         {
             return dto.IconStyle switch
             {
                 IconStyle.OldDrop => CreateUri(dto.IconStyle, dto.IconColor),
-                IconStyle.OldCircle => new { icon = CreateUri(dto.IconStyle, dto.IconColor), color = dto.IconColor, strokeColor = dto.IconColor, scale = 1 },
+                /// google.maps.SymbolPath.CIRCLE = 0, google.maps.SymbolPath.FORWARD_CLOSED_ARROW = 2, 
+                IconStyle.OldCircle => new { path = 0, fillColor = HexToRgb(dto.IconColor), fillOpacity = 1, strokeWeight = 1, strokeOpacity = 0.7, scale = 6 },                
                 IconStyle.Drop => CreateUri(dto.IconStyle, dto.IconColor),
                 IconStyle.Circle => CreateUri(dto.IconStyle, dto.IconColor),
                 IconStyle.Point => CreateUri(dto.IconStyle, dto.IconColor),
@@ -63,7 +63,7 @@ namespace OohelpWebApps.Presentations.Mapping
             };
         }
 
-        private static string GenerateHtmlInfo(Domain.Data.BoardDto dto)
+        private static string GenerateHtmlInfo(BoardDto dto)
         {
             StringBuilder sb = new StringBuilder("<div class=\"informWindow\"><strong>");
             sb.Append(dto.Code).Append(' ').Append(dto.Type).Append(' ').Append(dto.Size);
@@ -79,7 +79,6 @@ namespace OohelpWebApps.Presentations.Mapping
                 IconStyle.Drop => new Uri(string.Format(ICON_DROP_BASE_LINK, color)),
                 IconStyle.OldDrop => new Uri(ICON_OLD_DROP_BASE_LINK),
                 IconStyle.Circle => new Uri(string.Format(ICON_CIRCLE_BASE_LINK, color)),
-                IconStyle.OldCircle => new Uri(ICON_OLD_CIRCLE_BASE_LINK),
                 IconStyle.Point => new Uri(string.Format(ICON_POINT_BASE_LINK, color)),
                 IconStyle.Billboard => new Uri(string.Format(ICON_ROUNDED_BILLBOARD_BASE_LINK, color)),
 
